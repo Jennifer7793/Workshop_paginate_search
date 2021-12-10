c4d108048e735e5e8ccb876b2f0c404d796a9d57
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-10 11:20:33 +0800

    info_in_body

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(function(infor) {
       infor.forEach(element => {
-        // const content = document.querySelector('tbody')
-        // content.innerHTML += `
-        //   <tr>
-        //     <td>${element.rank}</td>
-        //     <td><a href="${element.link}">${element.title}</a></td>
-        //     <td>${element.year}</td>
-        //     <td>${element.rating}</td>
-        //     <td>${element.director}</td>
-        //     <td>${element.main_stars}</td>
-        //   </tr> 
-        //   `
+        const content = document.querySelector('tbody')
+        content.innerHTML += `
+          <tr>
+            <td>${element.rank}</td>
+            <td><a href="${element.link}">${element.title}</a></td>
+            <td>${element.year}</td>
+            <td>${element.rating}</td>
+            <td>${element.director}</td>
+            <td>${element.main_stars}</td>
+          </tr> 
+          `
         console.log(element);
       });
     })
