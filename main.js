f29cd59d843bf6b67919cf78c588b60bd4e672cc
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-10 11:18:01 +0800

    cl_info

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Start !
+  fetch('https://top-250-movies.herokuapp.com/api/v1/movies/top')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(infor) {
+      infor.forEach(element => {
+        // const content = document.querySelector('tbody')
+        // content.innerHTML += `
+        //   <tr>
+        //     <td>${element.rank}</td>
+        //     <td><a href="${element.link}">${element.title}</a></td>
+        //     <td>${element.year}</td>
+        //     <td>${element.rating}</td>
+        //     <td>${element.director}</td>
+        //     <td>${element.main_stars}</td>
+        //   </tr> 
+        //   `
+        console.log(element);
+      });
+    })
 })
